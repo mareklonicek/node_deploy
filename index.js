709cd7f3661528d606f1a8d30a70520cdba87e7b
Marek Loníček <mark.lonicek@seznam.cz>
2025-02-22 18:33:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ const app = express();
 const PORT = process.env.PORT || 8081;
 
 app.get('/', async (req, res) => {
-  const username = req.query.username || 'myogeshchavan97';
+  const username = req.query.username || 'mareklonicek';
   try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
+      try {
+    // const result = await axios.get(
+    //   `https://api.github.com/users/${username}/repos`
+    // );
+    // const repos = result.data
+    //   .map((repo) => ({
+    //     name: repo.name,
+    //     url: repo.html_url,
+    //     description: repo.description,
+    //     stars: repo.stargazers_count
+    //   }))
+    //   .sort((a, b) => b.stars - a.stars);
 
-    res.send(repos);
+    res.send("jxcnvjifdvjfdbhvdfvjfdbhjfdbhjdfbhj");
   } catch (error) {
     res.status(400).send('Error while getting list of repositories');
   }
