bb5d46c0a11961b05ca43530ac0225c16e80a19f
Marek Loníček <mark.lonicek@seznam.cz>
2025-02-22 18:24:50 +0100

    m

--- index.js
+++ index.js
@@ -1,96 +1,29 @@
-const express = require("express");
-const path = require("path");
-const bp = require("body-parser");
-const cors = require("cors");
-
-const { open } = require("sqlite");
-const sqlite3 = require("sqlite3");
-const bcrypt = require("bcrypt");
-
+const express = require('express');
+const axios = require('axios');
 const app = express();
-app.use(express.json());
+const PORT = process.env.PORT || 8081;
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
-app.use(
-  cors({
-    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-  })
-);
-
-app.use(bp.json());
-app.use(bp.urlencoded({ extended: true }));
-
-const dbPath = path.join(__dirname, "goodreads.db");
-
-let db = null;
-
-const port = process.env.PORT || 3001;
-
-const initializeDBAndServer = async () => {
+app.get('/', async (req, res) => {
+  const username = req.query.username || 'myogeshchavan97';
   try {
-    db = await open({
-      filename: dbPath,
-      driver: sqlite3.Database,
-    });
-    app.listen(port, () => {
-      console.log("Server Running at http://localhost:3000/");
-    });
-  } catch (e) {
-    console.log(`DB Error: ${e.message}`);
-    process.exit(1);
-  }
-};
-initializeDBAndServer();
+    const result = await axios.get(
+      `https://api.github.com/users/${username}/repos`
+    );
+    const repos = result.data
+      .map((repo) => ({
+        name: repo.name,
+        url: repo.html_url,
+        description: repo.description,
+        stars: repo.stargazers_count
+      }))
+      .sort((a, b) => b.stars - a.stars);
 
-// User Register API
-app.post("/users/", async (request, response) => {
-  const { username, name, password, gender, location } = request.body;
-  const hashedPassword = await bcrypt.hash(request.body.password, 10);
-  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
-  const dbUser = await db.get(selectUserQuery);
-  if (dbUser === undefined) {
-    const createUserQuery = `
-      INSERT INTO 
-        user (username, name, password, gender, location) 
-      VALUES 
-        (
-          '${username}', 
-          '${name}',
-          '${hashedPassword}', 
-          '${gender}',
-          '${location}'
-        )`;
-    const dbResponse = await db.run(createUserQuery);
-    const newUserId = dbResponse.lastID;
-    response.send(`Created new user with ${newUserId}`);
-  } else {
-    response.status = 400;
-    response.send("User already exists");
+    res.send(repos);
+  } catch (error) {
+    res.status(400).send('Error while getting list of repositories');
   }
 });
 
-
-app.post("/login", async (request, response) => {
-  const { username, password } = request.body;
-  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
-  const dbUser = await db.get(selectUserQuery);
-  if (dbUser === undefined) {
-    response.status(400);
-    response.send("Invalid User");
-  } else {
-    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
-    if (isPasswordMatched === true) {
-      response.send("Login Success!");
-    } else {
-      response.status(400);
-      response.send("Invalid Password");
-    }
-  }
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
-
-module.exports = app;
